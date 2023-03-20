28645661a2a15ef4f0baa79ccba101160d07593c
MandySpace <amandeepsingh1994@gmail.com>
2023-03-20 17:07:36 +0530

    Private routing logic change

--- src/uiLibrary/PrivateRoute.svelte
+++ src/uiLibrary/PrivateRoute.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
   import { navigate, Route } from "svelte-routing";
+  import Login from "../routes/Login.svelte";
 
   export let path: string;
+  let renderPrivateRoute = false;
 
-  if (!window.localStorage.getItem("token")) {
-    navigate("login");
+  $: if (!window.localStorage.getItem("token")) {
+    navigate("/login");
+  } else {
+    renderPrivateRoute = true;
   }
 </script>
 
 <Route {path}>
-  <slot />
+  {#if renderPrivateRoute}
+    <slot />
+  {:else}
+    <Login />
+  {/if}
 </Route>
