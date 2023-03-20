176a6837544f31965c67a91ee848f4c4abcc446b
MandySpace <amandeepsingh1994@gmail.com>
2023-03-20 16:38:24 +0530

    No on mounting for private route redirection

--- src/uiLibrary/PrivateRoute.svelte
+++ src/uiLibrary/PrivateRoute.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
   import { navigate, Route } from "svelte-routing";
-  import { onMount } from "svelte";
 
   export let path: string;
 
-  onMount(() => {
-    if (!window.localStorage.getItem("token")) {
-      navigate("login");
-    }
-  });
+  if (!window.localStorage.getItem("token")) {
+    navigate("login");
+  }
 </script>
 
 <Route {path}>
